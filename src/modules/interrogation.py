07eb7b396f5dd5e1224ea9ccd08d3f2e6157f264
Jeter Lin <43425262+k1ngjet3r@users.noreply.github.com>
2022-09-13 17:35:39 +0800

    finialized modules

--- src/modules/interrogation.py
+++ src/modules/interrogation.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import pyaudio
+import time 
+
 
 class Interrogation:
     """
@@ -24,11 +26,16 @@ class Interrogation:
         
         with self.mic as source:
             # Adjust the mic with teh ambinet noise
-            self.recognizer.adjust_for_ambient_noise(source)
-            audio = self.recognizer.listen(source)
+            print('adjusting the noise')
+            self.recognizer.adjust_for_ambient_noise(source, duration=1)
+            print('Finished! count down in 5')\
+
+            time.sleep(5)
+            print('listening...')
+            audio = self.recognizer.listen(source, timeout=20, phrase_time+)
 
         try:
-            response = self.recognizer.recognize_sphinx(audio)
+            return self.recognizer.recognize_sphinx(audio)
         except sr.RequestError:
             # API was unreachable or unresponsive
             print('API Unavailible')
@@ -36,7 +43,6 @@ class Interrogation:
             # Speech was unintelligible
             print("Fail to recognize the phrase!")
         
-        return response
 
 if __name__ == '__main__':
-    pass
+    print(Interrogation(mic_index=2).listen_from_mic())
